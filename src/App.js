5ed9f237985d6baa3313ba9226c48d8999c61cd7
Lucas Lima <lucasaraujo1994@gmail.com>
2023-01-18 21:23:22 +0000

    React Test 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá lucas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [name, setName] = useState();
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+  return(
+    <div>
+      {name ? `O meu nome é ${name}.` : `Eu estou Anonimo!`}
+      <hr/>
+      <input type="text" value={name} onChange={handleChange}/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
